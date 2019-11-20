080e4cbbe79e994e36395bae78844109f4379af4
Twittytop <169054211@qq.com>
2019-11-21 00:57:09 +0800

    chore(nav): 导航移到单独的目录管理

---
+++ docs/.vuepress/nav/en.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { text: 'Guide', link: '/guide/', ariaLabel: 'Guide' },
+  { text: 'Build Tools', ariaLabel: 'Build Tools', items: [
+      { text: 'Webpack', link: '/buildTools/webpack/' },
+      { text: 'Rollup', link: '/buildTools/rollup/' }
+    ] 
+  },
+  { text: 'Life', link: '/life/', ariaLabel: 'Life' }
+];
---
+++ docs/.vuepress/nav/zh.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { text: '指南', link: '/guide/' },
+  { text: '构建工具', ariaLabel: 'Build Tools', items: [
+      { text: 'Webpack', link: '/buildTools/webpack/' },
+      { text: 'Rollup', link: '/buildTools/rollup/' },
+    ] 
+  },
+  { text: '生活', link: '/life/' }
+];
