39442d68c7e864207ab64ec677f6c1cc29569171
Twittytop <169054211@qq.com>
2019-11-21 01:04:32 +0800

    chore(nav): 修改导航栏配置

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -1,8 +1,8 @@
 module.exports = [
   { text: 'Guide', link: '/guide/', ariaLabel: 'Guide' },
   { text: 'Build Tools', ariaLabel: 'Build Tools', items: [
-      { text: 'Webpack', link: '/buildTools/webpack/' },
-      { text: 'Rollup', link: '/buildTools/rollup/' }
+      { text: 'Webpack', link: '/buildTools/webpack/', ariaLabel: 'Webpack' },
+      { text: 'Rollup', link: '/buildTools/rollup/', ariaLabel: 'Rollup' }
     ] 
   },
   { text: 'Life', link: '/life/', ariaLabel: 'Life' }
