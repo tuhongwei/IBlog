1a19486f0992e7d70d78a172e36cc736b166ba0e
Twittytop <169054211@qq.com>
2019-11-21 00:57:36 +0800

    chore(config): 修改导航栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,6 +17,11 @@ module.exports = {
   },
   // 配置主题的语言
   themeConfig: {
+    repo: 'tuhongwei/IBlog',
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    editLinks: true,
     smoothScroll: true,
     locales: {
       '/': {
@@ -24,8 +29,10 @@ module.exports = {
         selectText: '选择语言',
         // 该语言在下拉菜单中的标签
         label: '简体中文',
+        ariaLabel: "选择语言",
         // 编辑链接文字
         editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
         // Service Worker 的配置
         serviceWorker: {
           updatePopup: {
@@ -35,9 +42,7 @@ module.exports = {
         },
         // 当前 locale 的 algolia docsearch 选项
         algolia: {},
-        nav: [
-          { text: '指南', link: '/guide/' }
-        ],
+        nav: require('./nav/zh'),
         sidebar: {
           '/': [/* ... */],
           '/guide/': [/* ... */]
@@ -48,6 +53,7 @@ module.exports = {
         label: 'English',
         ariaLabel: 'Languages',
         editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
         serviceWorker: {
           updatePopup: {
             message: "New content is available.",
@@ -55,9 +61,7 @@ module.exports = {
           }
         },
         algolia: {},
-        nav: [
-          { text: 'Guide', link: '/guide/', ariaLabel: 'Guide' }
-        ],
+        nav: require('./nav/en'),
         sidebar: {
           '/en/': [/* ... */],
           '/en/guide/': [/* ... */]
