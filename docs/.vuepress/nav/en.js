72ca8bcf4e74c020b4d52f446b4134546073ef53
Twittytop <hongwei.tu@chukong-inc.com>
2019-11-29 10:52:32 +0800

    feat(nav): 增加导航栏目

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -1,9 +1,16 @@
 module.exports = [
   { text: 'Guide', link: '/guide/', ariaLabel: 'Guide' },
-  { text: 'Build Tools', ariaLabel: 'Build Tools', items: [
-      { text: 'Webpack', link: '/buildTools/webpack/', ariaLabel: 'Webpack' },
-      { text: 'Rollup', link: '/buildTools/rollup/', ariaLabel: 'Rollup' }
-    ] 
-  },
+  { text: 'Ecotope', ariaLabel: 'Ecotope', items: [
+    { text: 'Vue', link: '/ecotope/vue/', ariaLabel: 'Vue' },
+    { text: 'React', link: '/ecotope/React/', ariaLabel: 'React' }
+  ]},
+  { text: 'Engineering', ariaLabel: 'Engineering', items: [
+    { text: 'Build Tools', ariaLabel: 'Build Tools', items: [
+      { text: 'Webpack', link: '/engineering/buildTools/webpack/', ariaLabel: 'Webpack' },
+      { text: 'Rollup', link: '/engineering/buildTools/rollup/', ariaLabel: 'Rollup' }
+    ]},
+    { text: 'CI', link: '/engineering/ci/', ariaLabel: 'CI' },
+  ]},
+  { text: 'Specification', link: '/specification/', ariaLabel: 'Specification' },
   { text: 'Life', link: '/life/', ariaLabel: 'Life' }
 ];
--- docs/.vuepress/nav/zh.js
+++ docs/.vuepress/nav/zh.js
@@ -1,9 +1,16 @@
 module.exports = [
   { text: '指南', link: '/guide/', ariaLabel: 'Guide' },
-  { text: '构建工具', ariaLabel: 'Build Tools', items: [
-      { text: 'Webpack', link: '/buildTools/webpack/', ariaLabel: 'Webpack' },
-      { text: 'Rollup', link: '/buildTools/rollup/', ariaLabel: 'Rollup' },
-    ] 
-  },
+  { text: '生态', ariaLabel: 'Ecotope', items: [
+    { text: 'Vue', link: '/ecotope/vue/', ariaLabel: 'Vue' },
+    { text: 'React', link: '/ecotope/React/', ariaLabel: 'React' }
+  ]},
+  { text: '工程化', ariaLabel: 'Engineering', items: [
+    { text: '构建工具', ariaLabel: 'Build Tools', items: [
+      { text: 'Webpack', link: '/engineering/buildTools/webpack/', ariaLabel: 'Webpack' },
+      { text: 'Rollup', link: '/engineering/buildTools/rollup/', ariaLabel: 'Rollup' }
+    ]},
+    { text: '持续集成', link: '/engineering/ci/', ariaLabel: 'CI' },
+  ]},
+  { text: '规范', link: '/specification/', ariaLabel: 'Specification' },
   { text: '生活', link: '/life/', ariaLabel: 'Life' }
 ];
